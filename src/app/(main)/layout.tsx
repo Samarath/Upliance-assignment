55f09505378d6354d94ab50b4b500bb328c74d5c
samrath kumar <samrathkumarmuz@gmail.com>
2025-02-05 23:22:31 +0530

    imported dynmaic coutner to resolved the error

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,9 +4,12 @@ import { Provider } from "react-redux";
 import { store } from "../../redux/store";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./page";
-import Counter from "./counter/page";
+// import Counter from "./counter/page";
 import UserForm from "./user-form/page";
 import TextEditor from "./text-editor/page";
+import dynamic from "next/dynamic";
+
+const Counter = dynamic(() => import("../(main)/counter/page"), { ssr: false });
 
 export default function AppLayout() {
   return (
