7e51b7ea6e4e4dbf371f348277c77160f18cdb42
samrath kumar <samrathkumarmuz@gmail.com>
2025-02-05 22:01:48 +0530

    set up routes

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,12 +2,21 @@
 
 import { Provider } from "react-redux";
 import { store } from "../../redux/store";
-import { ReactNode } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./page";
 
-export default function AppLayout({ children }: { children: ReactNode }) {
+export default function AppLayout() {
   return (
     <Provider store={store}>
-      <section>{children}</section>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/counter" element={<h1>counter component</h1>} />
+          <Route path="/user-form" element={<h1>form component</h1>} />
+          <Route path="/text-editor" element={<h1>text editor component</h1>} />
+        </Routes>
+        {/* <section>{children}</section> */}
+      </Router>
     </Provider>
   );
 }
